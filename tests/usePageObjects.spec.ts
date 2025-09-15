298c3c24114b4c5b3ffee1370b75f90873dd1640
Dmytro Zhuravlov <dima9696.91@gmail.com>
2025-09-15 19:29:32 -0400

    update Playwright workflow to run tests on Firefox and modify test to run only for form page

--- tests/usePageObjects.spec.ts
+++ tests/usePageObjects.spec.ts
@@ -6,7 +6,7 @@ test.beforeEach(async({page}) => {
     await page.goto('/')
 })
 
-test('navigate to form page', async({page}) => {
+test.only('navigate to form page', async({page}) => {
     const pm = new PageManager(page)
     await pm.navigateTo().formLayoutsPage()
     await pm.navigateTo().datePickerPage()
